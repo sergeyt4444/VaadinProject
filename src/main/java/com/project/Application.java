a37bc9d703819052c603b1a1562c3598399a5bf5
sergeyt4444 <sergeyt4444@rambler.ru>
2022-01-28 04:22:34 +0300

    config for eureka added

--- src/main/java/com/project/Application.java
+++ src/main/java/com/project/Application.java
@@ -1,11 +1,22 @@
 package com.project;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
