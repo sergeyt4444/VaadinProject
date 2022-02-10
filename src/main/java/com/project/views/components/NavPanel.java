2168065e5dfff6a7540a846e1f76c2a5fc53da53
sergeyt4444 <sergeyt4444@rambler.ru>
2022-02-10 03:57:50 +0300

    style improvements

--- src/main/java/com/project/views/components/NavPanel.java
+++ src/main/java/com/project/views/components/NavPanel.java
@@ -19,18 +19,23 @@ public class NavPanel extends VerticalLayout {
         mainPageButton.addClickListener(click -> {
             System.out.println("goto mainpage");
         });
+        mainPageButton.setClassName("nav-button");
         catPageButton = new Button("Категории");
         catPageButton.addClickListener(click -> {
         });
+        catPageButton.setClassName("nav-button");
         recentCourcesButton = new Button("Новые тренинги");
         recentCourcesButton.addClickListener(click -> {
         });
+        recentCourcesButton.setClassName("nav-button");
         courseCalendarButton = new Button("Запланированные курсы");
         courseCalendarButton.addClickListener(click -> {
         });
+        courseCalendarButton.setClassName("nav-button");
         contactPageButton = new Button("Обратная связь");
         contactPageButton.addClickListener(click -> {
         });
+        contactPageButton.setClassName("nav-button");
 
         add(mainPageButton, catPageButton, recentCourcesButton, courseCalendarButton, contactPageButton);
     }
